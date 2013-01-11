8e0cdf1824d05430f3a5e7ccd905178ca3a7db59
Radomír Černoch <radomir.cernoch@gmail.com>
2013-01-11 16:46:22 +0100

    Queriable adaptor with crossvalidation

--- src/main/scala/cernoch/sm/space/QueriableAdaptor.scala
+++ src/main/scala/cernoch/sm/space/QueriableAdaptor.scala
@@ -2,6 +2,7 @@ package cernoch.sm.space
 
 import cernoch.scalogic._
 import storage.Queriable
+import collection.mutable.ArrayBuffer
 
 /**
  * Adapts a queriable storage for [[cernoch.sm.space.bumphunt.BeamHistSearch]]
@@ -12,8 +13,61 @@ class QueriableAdaptor(storage: Queriable[Horn[Atom[FFT], Set[Atom[FFT]]], Val[_
     extends (Horn[HeadAtom, Set[Atom[FFT]]] => Iterable[Iterable[BigDecimal]]) {
 
   def apply(q: Horn[HeadAtom, Set[Atom[FFT]]])
-  = storage.query(q) // Execute the query
-    .groupBy{ _(q.head.exVar) }.values // Group by the example number
-    .map{ _.map{ _( q.head.histVar ).get.asInstanceOf[BigDecimal] } }
-    // And extract the histogram values
+  = QueriableAdaptor.helper(
+      storage.query(q) // Execute the query
+      .groupBy{ _(q.head.exVar) }.values, // Group by the example number
+    q.head.histVar) // And extract the histogram values
+
+}
+
+private object QueriableAdaptor {
+
+  private[space] def helper(i: Iterable[Iterable[Map[Var,Val[_]]]], v: Var)
+  = i.map{ _.map{ _( v ).get.asInstanceOf[BigDecimal] } }
+}
+
+
+
+/**
+ * Adapts a queriable storage for [[cernoch.sm.space.bumphunt.BeamHistSearch]]
+ * and splits data between train and test sets.
+ *
+ *
+ * @author Radomír Černoch (radomir.cernoch at gmail.com)
+ */
+class QueryCrossvalAdaptor
+  ( storage: Queriable[Horn[Atom[FFT], Set[Atom[FFT]]], Val[_]],
+    crossvalidator: (Iterable[_] => Iterable[(_) => Boolean])
+  )
+  extends (Horn[HeadAtom, Set[Atom[FFT]]]
+    => Iterable[(Iterable[Iterable[BigDecimal]],
+                 Iterable[Iterable[BigDecimal]] )] ) {
+
+  def apply(q: Horn[HeadAtom, Set[Atom[FFT]]])
+  = {
+    val data = storage.query(q) // Execute the query
+      .groupBy{ _(q.head.exVar) } // Group by the example number
+
+    crossvalidator(data).map( trainTest => {
+      val (train,test) = split(data)(trainTest)
+
+      ( QueriableAdaptor.helper(train.map{_._2}, q.head.histVar),
+        QueriableAdaptor.helper( test.map{_._2}, q.head.histVar) )
+    })
+  }
+
+  private def split[T]
+    (coll: Iterable[T])
+    (how: (T => Boolean))
+  : (Iterable[T],Iterable[T])
+  = {
+    val a = ArrayBuffer[T]()
+    val b = ArrayBuffer[T]()
+
+    for (x <- coll) yield how(x) match {
+      case true  => a += x
+      case false => b += x
+    }
+    (a,b)
+  }
 }
